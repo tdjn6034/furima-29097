78936ebbc95e12c7f09e13459da9e51296316a24
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2020-12-09 16:01:51 +0900

    Userテーブルへのデータ保存機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :last_name, presence: true
+  
 end
