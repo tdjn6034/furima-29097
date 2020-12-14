5fc1e5fabd74f075e20dde452c3641e88dc6f24e
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2020-12-15 06:32:18 +0900

    新規登録バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :last_name, presence: true
+  
   validates :nickname, presence: true
-  validates :email, uniqueness: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX}
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, presence: true, length: { minimum: 6 }, format: {with: VALID_PASSWORD_REGEX }
+  validates :password_confirmation ,presence: true
+
+  with_options presence: true do
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
+  
+  validates :birthday ,presence: true
+
+  has_many :items
+  has_many :purchase_records
   
 end
