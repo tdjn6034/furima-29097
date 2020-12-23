3a5a45a0c59e75f9e9d218936d3205420407b95d
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2020-12-23 17:40:46 +0900

    itemのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :product_description, presence: true
+  validates :category_id, presence: true
+  validates :quality_id, presence:true
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equel_to:9999999}
+
+
   validates :category, numericality: { other_than: 1 }
 end
