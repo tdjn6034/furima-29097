61b44ed149d6bbc1a0d3d814e7ab7a406403f9aa
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2020-12-23 13:48:44 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :product_description, :category_id, :quality_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, :price,:user,:image)
+  end
 end
