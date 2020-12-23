d72d24496e27d47b47e1f1360ec555188fbda0e3
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2020-12-23 19:33:23 +0900

    categoryモデルへのデータの登録

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: 'レディース' },
@@ -13,8 +13,8 @@ class Genre < ActiveHash::Base
     { id: 11, name: 'その他' }
   ]
 
-  include ActiveHash::Associations
-  has_many :articles
+    include ActiveHash::Associations
+    has_many :items
 
-  
+
   end
