9405a73912915113fe8f364ea883363b30db8913
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2020-12-23 16:40:19 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category, numericality: { other_than: 1 }
 end
