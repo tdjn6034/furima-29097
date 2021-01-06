b5f6e557885ccee5ae9f17e3c4987893ec9d0ddf
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2021-01-07 05:49:30 +0900

    Formオブジェクト作成前の状態に戻す

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname, presence: true
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
 
+  with_options presence: true do
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  end
+
+  validates :birthday, presence: true
 
   has_many :items
   has_many :purchase_records
