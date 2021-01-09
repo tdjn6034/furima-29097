79ac40829cdec7b46664f4423ab4477c763edf2d
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2021-01-09 15:37:14 +0900

    購入機能に必要なモデル、コントローラーの作成

---
+++ app/controllers/purchase_records_controller.rb
@@ -0,0 +1,10 @@
+class PurchaseRecordsController < ApplicationController
+
+  def index
+    @purchaserecord_address = PurchaserecordAddress.new 
+  end
+
+  def create
+  end
+
+end
---
+++ app/models/purchase_record.rb
@@ -0,0 +1,7 @@
+class PurchaseRecord < ApplicationRecord
+
+  belongs_to :item
+  has_one :address
+  belongs_to :user
+  
+end
