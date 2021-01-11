4c0876fc4cb0546f7a4c7d711236891d068612a8
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2021-01-11 09:48:25 +0900

    住所情報に関するエラー文の表示

--- app/models/purchaserecord_address.rb
+++ app/models/purchaserecord_address.rb
@@ -4,6 +4,7 @@ class PurchaserecordAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: " Input correctly" }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :municipality
     validates :address
     validates :phone_number, numericality: { only_integer: true, message: "Input only number"}
