4d6e8ab49e0c47c25fbe351a00ae063f95496749
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2021-01-13 18:49:03 +0900

    重複記述をbefore_actionにまとめる

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,14 +1,13 @@
 class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!
+  before_action :create_item
 
   def index
     @purchaserecord_address = PurchaserecordAddress.new
-    @item = Item.find(params[:item_id])
     redirect_to :root if current_user == @item.user || @item.purchase_record.present?
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchaserecord_address = PurchaserecordAddress.new(purchaserecord_params)
     if @purchaserecord_address.valid?
       Payjp.api_key = ENV['PAYJP_SECRET_KEY']
@@ -25,6 +24,10 @@ class PurchaseRecordsController < ApplicationController
   end
 
   private
+
+  def create_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchaserecord_params
     params.require(:purchaserecord_address).permit(:hoge, :user, :item, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_record_id).merge(user_id: current_user.id, item_id: params.require(:item_id), token: params[:token])
