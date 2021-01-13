a36e4601e2abe492225a5192829c223d5eba7587
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2021-01-13 14:19:31 +0900

    エラー分の不要なスペースの削除

--- app/models/purchaserecord_address.rb
+++ app/models/purchaserecord_address.rb
@@ -4,7 +4,7 @@ class PurchaserecordAddress
   validates :token, presence:true
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: " Input correctly" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :municipality
     validates :address
