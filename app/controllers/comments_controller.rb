eda1c394b67e2068fe2a6726e552a7b45beb40e1
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2021-01-30 09:32:10 +0900

    rubocopの実行

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,16 @@
 class CommentsController < ApplicationController
-
   def new
     @comments = Comment.all
-    @comment = Comment.new 
+    @comment = Comment.new
   end
-  
+
   def create
     @comment = Comment.new(comment_params)
-    if @comment.save
-      ActionCable.server.broadcast 'comment_channel', content: @comment, username: @comment.user.nickname
-    end
+    ActionCable.server.broadcast 'comment_channel', content: @comment, username: @comment.user.nickname if @comment.save
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
